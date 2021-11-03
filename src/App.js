aa655acc45ea2ef93334873f2c4160a5c1453441
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-03 10:13:27 -0600

    removed mui

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import './App.css';
-import Box from '@mui/material/Box';
-import NavBar from './components/NavBar';
-import Paper from '@mui/material/Paper';
-import Hero from './components/Hero';
 
 
 function App() {
     return (
         <div className='App'>
-            <Box
-                sx={{
-                    height: '100%',
-                    transform: 'translateZ(0px)',
-                    flexGrow: 1
-                }}
-            >
-                <NavBar/>
-                <Paper
-                    elevation={0}
-                    sx={{
-                        width: 1,
-                        px: 2,
-                        pt: 4,
-                        borderRadius: '16px 16px 0 0',
-                        mx: 'auto',
-                        pb: 4
-                    }}
-                >
-                    <Hero/>
-                </Paper>
-            </Box>
+
         </div>
     );
 }
