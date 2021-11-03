ddeeec867d0e6fe04146d27b11e8a67654833df4
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-03 14:54:43 -0600

    removed container from nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,22 +2,19 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Container } from '@mui/material';
 
 
 function NavBar() {
     return (
         <AppBar position='static'>
-            <Container>
-                <Toolbar>
-                    <Typography 
-                        variant='h6'
-                        component='div'
-                        sx={{ flexGrow: 1 }}>
-                        @logankimbs
-                    </Typography>
-                </Toolbar>
-            </Container>
+            <Toolbar>
+                <Typography 
+                    variant='h6'
+                    component='div'
+                    sx={{ flexGrow: 1 }}>
+                    @logankimbs
+                </Typography>
+            </Toolbar>
         </AppBar>
     );
 };
