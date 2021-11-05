249d056ba76934bb07d698ed9f4cda2f3cfc8fe6
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-05 15:12:36 -0600

    updated colors

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import { ReactComponent as FishingSvg } from '../images/fish.svg';
+import { createTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-
+import { blue } from '@mui/material/colors';
 
 function Hero() {
     return (
         <Box className='hero'>
-            <FishingSvg 
+            {/* <FishingSvg 
                 className='fishing-svg'
-                style={{ height: '26.5rem', width: '26.5rem' }}/>
+                style={{ height: '26.5rem', width: '26.5rem' }}/> */}
             <Box className='about'>
-                <Typography 
-                    variant='h6'
+                <Typography
+                    mb={1.5} 
+                    color={blue[400]}
+                    sx={{ fontFamily: 'monospace' }}
                     component="div">
-                    about me
+                    Hey, my name is
+                </Typography>
+                <Typography 
+                    variant='h4'
+                    mb={1.5}
+                    sx={{
+                        color: 'rgba(255,255,255, 0.8)'
+                    }}
+                    component="div">
+                    Logan Kimball.
+                </Typography>
+                <Typography 
+                    variant='h4'
+                    component="div"
+                    style={{
+                        color: 'rgba(255, 255, 255, 0.5)'
+                    }}
+                    mb={2}>
+                    I'm an aspiring software engineer.
                 </Typography>
                 <Typography
-                    color="text.secondary">
+                    style={{
+                        color: 'rgba(255, 255, 255, 0.5)'
+                    }}>
                     I love a good podcast, interesting conversation, and an unassuming win (especially in tennis).
                     Rick and Morty always make me laugh. Skateboarding, surfing, fishing, and mountain biking give me meaning.
                     I’m a big fan of yard work.
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,21 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { blue } from '@mui/material/colors';
 
 
 function NavBar() {
     return (
-        <AppBar position='static'>
+        <AppBar position='static'
+        color='transparent'
+        sx={{ boxShadow: 'none'}}>
             <Toolbar>
                 <Typography 
-                    variant='h6'
+                    color={blue[400]}
+                    variant='h5'
                     component='div'
                     sx={{ flexGrow: 1 }}>
-                    @logankimbs
+                    LK
                 </Typography>
             </Toolbar>
         </AppBar>
