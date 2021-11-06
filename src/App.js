68f643c534568abfcce4b6e48c3e80ae280a2282
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-06 09:15:29 -0600

    clean slate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import Container from '@mui/material/Container'
-import NavBar from './components/NavBar';
-import Hero from './components/Hero';
-import Resume from './components/Resume';
 
 function App() {
     return (
         <div className='App'>
-            <NavBar/>
-            <Container sx={{ pt: 4 }}>
-                <Hero/>
-                <Resume/>
-            </Container>
+
         </div>
     );
 }
