f6b340e4e848ded43a59fe3586766ea15bbe3d29
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-24 10:54:58 -0700

    build under construction page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,85 @@
+import * as React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import Resume from './resume.svg';
+import { styled } from '@mui/system';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import { ReactComponent as Logo } from "./logo.svg";
+
+const ResumeButton = styled(Button)({
+    color: 'rgb(96, 108, 56)',
+    '&:hover': {
+        backgroundColor: 'rgba(96, 108, 56, 0.158)'
+    },
+});
+
+const DownloadButton = styled(Button)({
+    position: "absolute",
+    color: 'rgb(96, 108, 56)',
+    bottom: 0,
+    width: "100%",
+    fontSize: "inherit",
+    '&:hover': {
+        backgroundColor: 'rgba(96, 108, 56, 0.158)'
+    },
+});
+
+const CloseButton = styled(IconButton)({
+    color: 'rgb(96, 108, 56)',
+    position: "absolute",
+    right: "8px",
+    top: "8px",
+    '&:hover': {
+        backgroundColor: 'rgba(96, 108, 56, 0.158)'
+    },
+});
 
 function App() {
+    const [open, setOpen] = React.useState(false);
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleToggle = () => {
+        setOpen(!open);
+    };
+    const download = () => {
+        const link = document.createElement("a");
+        link.download = 'resume.pdf';
+        link.href = './resume.pdf';
+        link.click();
+    };
+
     return (
-        <div className='App'>
-            <Header />
-            <main>
-                <Hero />
-                <About />
-            </main>
-        </div>
+        <main>
+            <Logo className="logo" />
+            <section className='hero'>
+                <h1 className='sirname'>Website is under construction</h1>
+                <p className='shortintro'>
+                    Sorry for the inconvience 🙁but I'm working my tail off to get this site up and running.
+                    In the mean time feel free to reach out with the provided contact information on my resume
+                    or message me on any of the apps below.
+                </p>
+                <ResumeButton onClick={handleToggle} size="large">View Resume</ResumeButton>
+            </section>
+            <Dialog open={open} onClose={handleClose}>
+                <img src={Resume} />
+                <CloseButton aria-label="close" size="small" onClick={handleClose}>
+                    <CloseIcon fontSize="inherit" />
+                </CloseButton>
+                <DownloadButton
+                    aria-label="close"
+                    sx={{
+                        p: 1
+                    }}
+                    onClick={download}
+                >
+                    Download
+                </DownloadButton>
+
+            </Dialog>
+        </main>
     );
 }
 
