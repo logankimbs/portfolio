b068a998592c15fb97d17b277d1c1c0b20407fb4
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-24 15:06:37 -0700

    added logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/system';
 
+const Logo = styled("img")({
+    position: "absolute",
+    width: 150,
+    zIndex: -1,
+})
+
 const ResumeButton = styled(Button)({
     color: "rgb(96, 108, 56)",
     "&:hover": {
@@ -21,23 +27,26 @@ function App() {
     };
 
     return (
-        <Box sx={{ mx: 2 }}>
-            <Box
-                sx={{
-                    mt: 10,
-                    mx: "auto",
-                    maxWidth: 650,
-                }}
-            >
-                <h1>Website is under construction</h1>
-                <p>
-                    Sorry for the inconvience. I'm working my tail off to get this things up and running for you.
-                    In the mean time feel free to reach out with the provided contact information on my resume. Hope
-                    to hear from you soon.
-                </p>
-                <ResumeButton onClick={download} size="large">Download Resume</ResumeButton>
+        <Box>
+            <Box sx={{ mx: 2 }}>
+                <Box
+                    sx={{
+                        pt: 10,
+                        mx: "auto",
+                        maxWidth: 650
+                    }}
+                >
+                    <Logo src="./logo.svg" />
+                    <h1>Website is under construction</h1>
+                    <p>
+                        Sorry for the inconvience. I'm working my tail off to get this things up and running for you.
+                        In the mean time feel free to reach out with the provided contact information on my resume. Hope
+                        to hear from you soon.
+                    </p>
+                    <ResumeButton onClick={download} size="large">Download Resume</ResumeButton>
+                </Box>
             </Box>
-        </Box >
+        </Box>
     );
 }
 
