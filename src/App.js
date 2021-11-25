6e1688a84722fc303bba4f8cfd1b63847c5a535e
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-25 07:58:26 -0700

    added logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import './module.App.css';
 import Button from '@mui/material/Button';
+import { ReactComponent as Logo } from "./logo.svg";
 import Box from '@mui/material/Box';
 import { styled } from '@mui/system';
 
 const ResumeButton = styled(Button)({
-    color: "rgb(96, 108, 56)",
+    color: "rgba(0, 0, 0, 0.8)",
     "&:hover": {
-        backgroundColor: "rgba(96, 108, 56, 0.089)"
+        backgroundColor: "rgba(0, 0, 0, 0.089)"
     }
 });
 
@@ -22,16 +23,16 @@ function App() {
 
     return (
         <main className="hero-container">
+            <Logo className="logo" />
             <Box className="hero">
                 <h1>Website is under construction</h1>
                 <p>
-                    Sorry for the inconvience. I'm working my tail off to get this things up and running for you.
-                    In the mean time feel free to reach out with the provided contact information on my resume. Hope
+                    Sorry for the inconvience.I'm working my tail off to get this things up and running for you.
+                    In the mean time feel free to reach out with the provided contact information on my resume.Hope
                     to hear from you soon.
                 </p>
                 <ResumeButton onClick={download} size="large">Download Resume</ResumeButton>
             </Box>
-
         </main >
     );
 }
