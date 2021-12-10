c47bf760df47c8c421b7b3023e8b2eb3e8007bde
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-12-10 13:31:16 -0700

    removed files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import theme from "./utilities/styles/theme";
+
 import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 )
