d093b397750e506f16dd1f1bc41da99e6e370321
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-12-11 10:34:00 -0700

    built basic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
-import "./utilities/styles/styles.css";
+import './utilities/styles/styles.css';
 
-import ContactForm from './components/ContactForm'
+import NavigationBar from './components/NavigationBar';
+
+const sections = ['About', 'Resume', 'Projects', 'Contact'];
 
 function App() {
-    return (
-        <ContactForm />
-    );
+    return <NavigationBar sections={sections} />;
 }
 
 export default App;
