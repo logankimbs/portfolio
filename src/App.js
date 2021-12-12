5dd34e64c656e7e1c77616aee47622ba54076df8
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-12-11 17:47:25 -0700

    added about section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,55 @@ import * as React from 'react';
 import './utilities/styles/styles.css';
 
 import NavigationBar from './components/NavigationBar';
+import Portrait from './utilities/images/bigsurtrip.jpg';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
 
 function App() {
     const sections = ['About', 'Resume', 'Projects', 'Contact'];
 
-    return <NavigationBar sections={sections} />;
+    return (
+        <>
+            <NavigationBar sections={sections} />
+            <Box component="main">
+                <Container maxWidth="md" id="About">
+                    <Typography variant="h2">About Me</Typography>
+                    <Grid container spacing={2} alignItems="flex-start">
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Typography variant="p" component="p">
+                                Contrary to popular belief, Lorem Ipsum is not
+                                simply random text. It has roots in a piece of
+                                classical Latin literature from 45 BC, making it
+                                over 2000 years old. Richard McClintock, a Latin
+                                professor at Hampden-Sydney College in Virginia,
+                                looked up one of the more obscure Latin words,
+                                consectetur, from a Lorem Ipsum passage, and
+                                going through the cites of the word in classical
+                                literature, discovered the undoubtable source.
+                            </Typography>
+                            <Typography variant="p" component="p">
+                                Contrary to popular belief, Lorem Ipsum is not
+                                simply random text. It has roots in a piece of
+                                classical Latin literature from 45 BC, making it
+                                over 2000 years old.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Box
+                                component="img"
+                                sx={{ width: '100%' }}
+                                alt="windy day in big sur"
+                                src={Portrait}
+                            />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </>
+    );
 }
 
 export default App;
--- src/utilities/styles/theme.js
+++ src/utilities/styles/theme.js
@@ -1,16 +1,28 @@
-import createTheme from "@mui/material/styles/createTheme";
-import blue from "@mui/material/colors/blue";
-import purple from "@mui/material/colors/purple";
+import createTheme from '@mui/material/styles/createTheme';
+import blue from '@mui/material/colors/blue';
+import purple from '@mui/material/colors/purple';
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: blue[500]
+    palette: {
+        primary: {
+            main: blue[500],
+        },
+        secondary: {
+            main: purple[500],
+        },
     },
-    secondary: {
-      main: purple[500]
-    }
-  }
+    typography: {
+        h2: {
+            fontSize: '1.875rem',
+            fontWeight: 700,
+            lineHeight: 1.5,
+            margin: '40px 0px 10px',
+        },
+
+        p: {
+            margin: '0px 0px 16px',
+        },
+    },
 });
 
 export default theme;
