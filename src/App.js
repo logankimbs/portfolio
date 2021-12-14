fae9c94e75ee38721225932262e134b43110272b
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-12-14 14:29:43 -0700

    working through the styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 
 import './utilities/styles/styles.css';
-import getTheme from './utilities/styles/theme';
+
+import useMediaQuery from '@mui/material/useMediaQuery';
+import createTheme from '@mui/material/styles/createTheme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import NavigationBar from './components/NavigationBar';
 import Portrait from './utilities/images/bigsurtrip.jpg';
@@ -18,200 +19,234 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import DownloadIcon from '@mui/icons-material/Download';
 
-function App() {
-    const presfersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-    const palleteMode = React.useMemo(
+export default function App() {
+    const [loading, setLoading] = React.useState(true);
+    const [projects, setProjects] = React.useState([]);
+
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+    const theme = React.useMemo(
         () =>
             createTheme({
                 palette: {
-                    mode: presfersDarkMode ? 'dark' : 'light',
+                    mode: prefersDarkMode ? 'dark' : 'light',
                 },
             }),
-        [presfersDarkMode]
+        [prefersDarkMode]
     );
-    const theme = createTheme(getTheme(palleteMode.palette.mode));
-    const sections = ['About', 'Projects', 'Contact'];
-
-    const [loading, setLoading] = React.useState(true);
-    const [repos, setRepos] = React.useState([]);
 
     React.useEffect(() => {
-        setLoading(true);
         fetch('https://api.github.com/users/logankimbs/repos')
             .then((res) => res.json())
-            .then((data) => {
-                setData(
-                    data.filter(
-                        (x) =>
-                            x.name === 'intex' ||
-                            x.name === 'portfolio' ||
-                            x.name === 'twitter-bots'
+            .then((projects) => {
+                setProjects(
+                    projects.filter(
+                        (project) =>
+                            project.name === 'intex' ||
+                            project.name === 'portfolio' ||
+                            project.name === 'twitter-bots'
                     )
                 );
             });
-    }, []);
-
-    const setData = (data) => {
-        setRepos(data);
         setLoading(false);
-    };
+    }, []);
 
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <NavigationBar sections={sections} />
-            <Box component="main">
-                {/* intro section */}
-                <Container
-                    component="section"
-                    maxWidth="sm"
-                    sx={{
-                        height: '100vh',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                    }}
-                >
-                    <Typography component="h1" variant="h1">
-                        Hey, I'm{' '}
-                        <Typography
-                            component="span"
-                            variant="h1"
-                            color="primary.main"
-                        >
-                            Logan Kimball
-                        </Typography>
-                    </Typography>
-                    <Typography component="p" variant="body1">
-                        over 2000 years old. Richard McClintock, a Latin
-                        professor at Hampden-Sydney College in Virginia, looked
-                        up one of the more obscure Latin words, consectetur,
-                        from a Lorem Ipsum passage, and going through the cites
-                        of the word in classical literature, discovered the
-                        undoubtable source.
-                    </Typography>
-
-                    <Button
-                        variant="contained"
-                        size="large"
-                        startIcon={<DownloadIcon />}
-                        sx={{
-                            textTransform: 'none',
-                            fontWeight: 700,
-                            fontSize: '1rem',
-                            py: 1.5,
-                        }}
-                    >
-                        download resume
-                    </Button>
-                </Container>
-
-                {/* about section */}
-                <Container
-                    component="section"
-                    maxWidth="md"
-                    id="About"
-                    sx={{ mb: '20vh' }}
-                >
-                    <Typography
-                        component="h2"
-                        variant="smallh2"
-                        color="primary"
-                    >
-                        About
-                    </Typography>
-                    <Typography component="h2" variant="h2">
-                        Just a little something special, but not too long{' '}
-                        <Typography
-                            component="span"
-                            variant="h2"
-                            color="primary.main"
-                        >
-                            software engineer
-                        </Typography>
-                    </Typography>
-                    <Grid container spacing={2} alignItems="flex-start">
-                        <Grid item xs={12} sm={12} md={6}>
-                            <Typography component="p" variant="body1">
-                                Contrary to popular belief, Lorem Ipsum is not
-                                simply random text. It has roots in a piece of
-                                classical Latin literature from 45 BC, making it
-                                over 2000 years old. Richard McClintock, a Latin
-                                professor at Hampden-Sydney College in Virginia,
-                                looked up one of the more obscure Latin words,
-                                consectetur, from a Lorem Ipsum passage, and
-                                going through the cites of the word in classical
-                                literature, discovered the undoubtable source.
-                            </Typography>
-                            <Typography component="p" variant="body1">
-                                Contrary to popular belief, Lorem Ipsum is not
-                                simply random text. It has roots in a piece of
-                                classical Latin literature from 45 BC, making it
-                                over 2000 years old.
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={6}>
-                            <Box
-                                component="img"
-                                sx={{ width: '100%' }}
-                                alt="windy day in big sur"
-                                src={Portrait}
-                            />
-                        </Grid>
-                    </Grid>
-                </Container>
-
-                {/* projects section */}
-                <Container
-                    component="section"
-                    maxWidth="md"
-                    id="Projects"
-                    sx={{ mb: '20vh' }}
-                >
-                    <Typography
-                        component="h2"
-                        variant="smallh2"
-                        color="primary"
-                    >
-                        Projects
-                    </Typography>
-                    <Typography component="h2" variant="h2" mb={2.5}>
-                        Just a little something special, but not too long about{' '}
-                        <Typography
-                            component="span"
-                            variant="h2"
-                            color="primary.main"
-                        >
-                            cool projects
-                        </Typography>
-                    </Typography>
-
-                    <Grid container spacing={2} alignItems="flex-start">
-                        {!loading
-                            ? repos.map((repo) => (
-                                  <Grid item xs={12} md={4} key={repo.id}>
-                                      <ProjectCard project={repo} />
-                                  </Grid>
-                              ))
-                            : null}
-                    </Grid>
-                </Container>
-
-                {/* contact section */}
-                <Container
-                    component="section"
-                    maxWidth="md"
-                    id="Contact"
-                    sx={{ mb: '50vh' }}
-                >
-                    <Typography component="h2" variant="h2">
-                        Contact
-                    </Typography>
-                    <ContactForm />
-                </Container>
-            </Box>
+            <NavigationBar />
         </ThemeProvider>
     );
+
+    // return (
+    //     <ThemeProvider theme={theme}>
+    //         <CssBaseline />
+    //         <NavigationBar />
+    //         <Box component="main">
+    //             {/* intro section */}
+    //             <Container
+    //                 component="section"
+    //                 maxWidth="sm"
+    //                 sx={{
+    //                     height: '100vh',
+    //                     display: 'flex',
+    //                     flexDirection: 'column',
+    //                     justifyContent: 'center',
+    //                 }}
+    //             >
+    //                 <Typography component="h1" variant="h1">
+    //                     Hey, I'm{' '}
+    //                     <Typography
+    //                         component="span"
+    //                         variant="h1"
+    //                         color="primary.main"
+    //                     >
+    //                         Logan Kimball
+    //                     </Typography>
+    //                 </Typography>
+    //                 <Typography component="p" variant="body1">
+    //                     over 2000 years old. Richard McClintock, a Latin
+    //                     professor at Hampden-Sydney College in Virginia, looked
+    //                     up one of the more obscure Latin words, consectetur,
+    //                     from a Lorem Ipsum passage, and going through the cites
+    //                     of the word in classical literature, discovered the
+    //                     undoubtable source.
+    //                 </Typography>
+
+    //                 <Button
+    //                     variant="contained"
+    //                     size="large"
+    //                     startIcon={<DownloadIcon />}
+    //                     sx={{
+    //                         textTransform: 'none',
+    //                         fontWeight: 700,
+    //                         fontSize: '1rem',
+    //                         py: 1.5,
+    //                     }}
+    //                 >
+    //                     download resume
+    //                 </Button>
+    //             </Container>
+
+    //             {/* about section */}
+    //             <Container
+    //                 component="section"
+    //                 maxWidth="md"
+    //                 id="About"
+    //                 sx={{ mb: '20vh' }}
+    //             >
+    //                 <Typography
+    //                     component="h2"
+    //                     variant="smallh2"
+    //                     color="primary"
+    //                 >
+    //                     About
+    //                 </Typography>
+    //                 <Typography component="h2" variant="h2">
+    //                     Just a little something special, but not too long{' '}
+    //                     <Typography
+    //                         component="span"
+    //                         variant="h2"
+    //                         color="primary.main"
+    //                     >
+    //                         software engineer
+    //                     </Typography>
+    //                 </Typography>
+    //                 <Grid container spacing={2} alignItems="flex-start">
+    //                     <Grid item xs={12} sm={12} md={6}>
+    //                         <Typography component="p" variant="body1">
+    //                             Contrary to popular belief, Lorem Ipsum is not
+    //                             simply random text. It has roots in a piece of
+    //                             classical Latin literature from 45 BC, making it
+    //                             over 2000 years old. Richard McClintock, a Latin
+    //                             professor at Hampden-Sydney College in Virginia,
+    //                             looked up one of the more obscure Latin words,
+    //                             consectetur, from a Lorem Ipsum passage, and
+    //                             going through the cites of the word in classical
+    //                             literature, discovered the undoubtable source.
+    //                         </Typography>
+    //                         <Typography component="p" variant="body1">
+    //                             Contrary to popular belief, Lorem Ipsum is not
+    //                             simply random text. It has roots in a piece of
+    //                             classical Latin literature from 45 BC, making it
+    //                             over 2000 years old.
+    //                         </Typography>
+    //                     </Grid>
+    //                     <Grid item xs={12} sm={12} md={6}>
+    //                         <Box
+    //                             component="img"
+    //                             sx={{ width: '100%' }}
+    //                             alt="windy day in big sur"
+    //                             src={Portrait}
+    //                         />
+    //                     </Grid>
+    //                 </Grid>
+    //             </Container>
+
+    //             {/* projects section */}
+    //             <Container
+    //                 component="section"
+    //                 maxWidth="md"
+    //                 id="Projects"
+    //                 sx={{ mb: '20vh' }}
+    //             >
+    //                 <Typography
+    //                     component="h2"
+    //                     variant="smallh2"
+    //                     color="primary"
+    //                 >
+    //                     Projects
+    //                 </Typography>
+    //                 <Typography component="h2" variant="h2" mb={2.5}>
+    //                     Just a little something special, but not too long about{' '}
+    //                     <Typography
+    //                         component="span"
+    //                         variant="h2"
+    //                         color="primary.main"
+    //                     >
+    //                         cool projects
+    //                     </Typography>
+    //                 </Typography>
+
+    //                 <Grid container spacing={2} alignItems="flex-start">
+    //                     {!loading
+    //                         ? repos.map((repo) => (
+    //                               <Grid item xs={12} md={4} key={repo.id}>
+    //                                   <ProjectCard project={repo} />
+    //                               </Grid>
+    //                           ))
+    //                         : null}
+    //                 </Grid>
+    //             </Container>
+
+    //             {/* contact section */}
+    //             <Container
+    //                 component="section"
+    //                 maxWidth="md"
+    //                 id="Contact"
+    //                 sx={{ mb: '50vh' }}
+    //             >
+    //                 <Typography component="h2" variant="h2">
+    //                     Contact
+    //                 </Typography>
+    //                 <ContactForm />
+    //             </Container>
+    //         </Box>
+    //     </ThemeProvider>
+    // );
 }
 
-export default App;
+// const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+// const theme = React.useMemo(
+//     () =>
+//         createTheme({
+//             palette: {
+//                 mode: prefersDarkMode ? 'dark' : 'light',
+//             },
+//         }),
+//     [prefersDarkMode]
+// );
+
+// const [loading, setLoading] = React.useState(true);
+// const [repos, setRepos] = React.useState([]);
+
+// React.useEffect(() => {
+//     setLoading(true);
+//     fetch('https://api.github.com/users/logankimbs/repos')
+//         .then((res) => res.json())
+//         .then((data) => {
+//             setData(
+//                 data.filter(
+//                     (x) =>
+//                         x.name === 'intex' ||
+//                         x.name === 'portfolio' ||
+//                         x.name === 'twitter-bots'
+//                 )
+//             );
+//         });
+// }, []);
+
+// const setData = (data) => {
+//     setRepos(data);
+//     setLoading(false);
+// };
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,10 @@
 import * as React from 'react';
 
+import styled from '@mui/material/styles/styled';
+import useTheme from '@mui/material/styles/useTheme';
+import createTheme from '@mui/material/styles/createTheme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+
 import Slide from '@mui/material/Slide';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import AppBar from '@mui/material/AppBar';
@@ -13,12 +18,10 @@ import Menu from '@mui/material/Menu';
 import { Link } from 'react-scroll';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
 
-export default function NavigationBar(props) {
-    const { sections } = props;
-
+export default function NavigationBar() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const sections = ['About', 'Projects', 'Contact'];
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
@@ -29,68 +32,45 @@ export default function NavigationBar(props) {
     };
 
     return (
-        <>
-            <Slide appear={false} direction="down" in={!useScrollTrigger()}>
-                <AppBar color="primary" sx={{ py: 1.5 }}>
-                    <Container maxWidth="lg">
-                        <Toolbar disableGutters>
-                            <Box sx={{ flexGrow: 1, mr: 2 }}>
-                                <Typography component="h6" variant="h6">
-                                    LK
-                                </Typography>
-                            </Box>
+        <Slide appear={false} direction="down" in={!useScrollTrigger()}>
+            <AppBar color="inherit" sx={{ py: 1.5 }} enableColorOnDark>
+                <Container maxWidth="lg">
+                    <Toolbar disableGutters>
+                        <Box sx={{ flexGrow: 1, mr: 2 }}>
+                            <Typography component="h6" variant="h6">
+                                LK
+                            </Typography>
+                        </Box>
 
-                            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                                <IconButton
-                                    size="large"
-                                    color="inherit"
-                                    aria-label="menu for main page"
-                                    aria-controls="menu-appbar"
-                                    aria-haspopup="true"
-                                    onClick={handleOpenNavMenu}
-                                >
-                                    <MenuIcon />
-                                </IconButton>
-                                <Menu
-                                    id="menu-appbar"
-                                    keepMounted
-                                    open={Boolean(anchorElNav)}
-                                    onClose={handleCloseNavMenu}
-                                    sx={{
-                                        display: { xs: 'block', md: 'none' },
-                                    }}
-                                    anchorEl={anchorElNav}
-                                    anchorOrigin={{
-                                        vertical: 'bottom',
-                                        horizontal: 'right',
-                                    }}
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
-                                >
-                                    {sections.map((section) => (
-                                        <Link
-                                            key={section}
-                                            to={section}
-                                            spy={true}
-                                            smooth={true}
-                                            onClick={handleCloseNavMenu}
-                                        >
-                                            <MenuItem
-                                                sx={{
-                                                    mb: 0,
-                                                    textAlign: 'right',
-                                                }}
-                                            >
-                                                {section}
-                                            </MenuItem>
-                                        </Link>
-                                    ))}
-                                </Menu>
-                            </Box>
-
-                            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                color="inherit"
+                                aria-label="menu for main page"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                keepMounted
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'right',
+                                }}
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                            >
                                 {sections.map((section) => (
                                     <Link
                                         key={section}
@@ -99,23 +79,43 @@ export default function NavigationBar(props) {
                                         smooth={true}
                                         onClick={handleCloseNavMenu}
                                     >
-                                        <Button
-                                            size="small"
+                                        <MenuItem
                                             sx={{
-                                                color: 'inherit',
-                                                display: 'block',
+                                                mb: 0,
+                                                textAlign: 'right',
                                             }}
                                         >
                                             {section}
-                                        </Button>
+                                        </MenuItem>
                                     </Link>
                                 ))}
-                            </Box>
-                        </Toolbar>
-                    </Container>
-                </AppBar>
-            </Slide>
-            <Divider />
-        </>
+                            </Menu>
+                        </Box>
+
+                        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                            {sections.map((section) => (
+                                <Link
+                                    key={section}
+                                    to={section}
+                                    spy={true}
+                                    smooth={true}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Button
+                                        size="small"
+                                        sx={{
+                                            color: 'inherit',
+                                            display: 'block',
+                                        }}
+                                    >
+                                        {section}
+                                    </Button>
+                                </Link>
+                            ))}
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Slide>
     );
 }
